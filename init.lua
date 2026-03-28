eaf485a32aab21ec5913bd30c7308391e1fa452c
Adam Coddington <me@adamcoddington.net>
2026-03-27 22:56:47 -0500

    Improve efficiency around detecting when plugin should be enabled.

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+print("Loading Aim Assist Mod")
+
 dofile_once("mods/aim_assist/files/scripts/lib/utilities.lua")
 
-function OnWorldPreUpdate()
-    local player = EntityGetWithTag("player_unit")[1]
-    if player == nil then return end
+function OnPlayerSpawned(player)
     local player_object = Player(player)
     local autoaim = player_object.autoaim
-    autoaim._enabled = GameGetIsGamepadConnected()
+    autoaim._enabled = GameGetIsGamepadConnected() or InputIsJoystickConnected(0)
+    print("Autoaim " .. (autoaim._enabled and "enabled" or "disabled") .. " for player " .. tostring(player))
 end
